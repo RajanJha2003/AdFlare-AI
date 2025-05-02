b89df4c9d1a9d617e9c10cbdb65025af90596e19
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-05-02 08:46:48 +0530

    user schema

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "./provider";
 
 
 
@@ -21,7 +22,9 @@ export default function RootLayout({ children }) {
       <body
         className={outfit.className}
       >
+        <Provider>
         {children}
+        </Provider>
       </body>
     </html>
     </ClerkProvider>
--- app/provider.jsx
+++ app/provider.jsx
@@ -7,7 +7,7 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 const Provider = ({children}) => {
     const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
   return (
-    <div>{children}</div>
+    <ConvexProvider client={convex}>{children}</ConvexProvider>
   )
 }
 
