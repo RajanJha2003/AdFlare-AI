4810402346e1ede9a2fe77fc261eb751db5e33ef
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-05-03 09:21:15 +0530

    generate script api

---
+++ app/api/generate-script/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server"
+import OpenAI from "openai"
+
+export  async function POST(req){
+   
+
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey:process.env.OPENROUTER_API_KEY ,
+ 
+})
+
+
+  const completion = await openai.chat.completions.create({
+    model: "google/gemini-2.0-flash-exp:free",
+    messages: [
+      { role: "user", content: "Say this is a test" }
+    ],
+  })
+
+  console.log(completion.choices[0].message)
+return NextResponse.json(completion.choices[0].message?.content)
+
+
+}
